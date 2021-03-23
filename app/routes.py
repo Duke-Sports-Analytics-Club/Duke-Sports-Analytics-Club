3773676dcc37c2054da9f710a5d3507573e3df3a
Caleb Woo <caleb.woo@duke.edu>
2021-03-22 23:44:32 -0400

    new menu, changed color scheme, one more bio, new logo

--- app/routes.py
+++ app/routes.py
@@ -2,22 +2,26 @@ from flask import render_template, request, url_for
 from app import app
 
 @app.route('/')
-@app.route('/home', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET'])
 def home():
     return render_template("home.html")
 
-@app.route('/about', methods=['GET', 'POST'])
+@app.route('/about', methods=['GET'])
 def about():
     return render_template("about.html")
 
-@app.route('/profiles', methods=['GET', 'POST'])
+@app.route('/profiles', methods=['GET'])
 def profiles():
     return render_template("profiles.html")
 
-@app.route('/projects', methods=['GET', 'POST'])
+@app.route('/projects', methods=['GET'])
 def projects():
     return render_template("projects.html")
 
-@app.route('/tutorial_post', methods=['GET', 'POST'])
+@app.route('/resources', methods=['GET'])
+def resources():
+    return render_template("resources.html")
+
+@app.route('/tutorial_post', methods=['GET'])
 def tutorial_post():
     return render_template("tutorial_post.html")
