4e75928e43fcb921d399d1deff9fa1e6cd47b6cb
Dillan Sant <dillan.sant@duke.edu>
2022-12-20 15:30:36 -0600

    port

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+import os
+
+port = int(os.environ.get("PORT", 5000))
 
 app = Flask(__name__, static_url_path='/static')
+
+app.run(host='0.0.0.0', port=port)
+
 
 from app import routes
